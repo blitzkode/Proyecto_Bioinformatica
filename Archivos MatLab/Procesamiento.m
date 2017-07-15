87b5d4437f1ef0f20a0784a999f2a596b805038d
Diego Baes <diegobaesv@gmail.com>
2017-07-15 14:41:07 -0500

    Funcion "imagetoArray" agregada a procesamiento.m

--- Archivos MatLab/Procesamiento.m
+++ Archivos MatLab/Procesamiento.m
@@ -1,4 +1,7 @@
-function M=imagetoVector(A)
-
-
+function M=ImagetoArray(A, n1, n2) 
+B = imresize (A, [n1 n2]) %ajusta las dimensiones a la imagen
+C=rgb2gray(B);  % conversión a escala de grises
+umbral=graythresh(C);  % umbral determinado por Matlab entre 0 y 1
+D=im2bw(C,umbral);  % conversión a imagen binaria
+M=D %retorna la matriz escalada en binario
 end
